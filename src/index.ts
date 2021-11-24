1c488355d77d116bd87900c97df613c4237620ee
Matheus Betim <mbetim47@gmail.com>
2021-11-23 21:51:08 -0300

    chore: install express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello world");
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const appPort = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World" });
+});
+
+app.listen(appPort, () => console.log(`Listening`));
