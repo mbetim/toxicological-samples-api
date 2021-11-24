738d0e0ca6ab457c4936da3892652d1fca53dfc2
Matheus Betim <mbetim47@gmail.com>
2021-11-23 21:55:12 -0300

    chore: instala e configura um servidor simples com express, morgan e helmet

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
+import morgan from "morgan";
+import helmet from "helmet";
 
 dotenv.config();
 
 const app = express();
 const appPort = process.env.PORT || 3000;
 
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.json({ message: "Hello World" });
 });
 
-app.listen(appPort, () => console.log(`Listening`));
+app.listen(appPort, () => console.log(`> Server started on port ${appPort}`));
