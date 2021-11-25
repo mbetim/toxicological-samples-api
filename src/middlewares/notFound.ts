47f118949e6786fa8db0ce6c11e8e84c7217ca0c
Matheus Betim <mbetim47@gmail.com>
2021-11-25 17:21:56 -0300

    fix: traduz a mensagem de erro para rotas não encontradas

--- src/middlewares/notFound.ts
+++ src/middlewares/notFound.ts
@@ -2,4 +2,4 @@ import { RequestHandler } from "express";
 import HttpException from "../utils/HttpException";
 
 export const notFound: RequestHandler = (req, res, next) =>
-  next(new HttpException(`Not found - ${req.originalUrl}`, 404));
+  next(new HttpException(`Endpoint não encontrado - ${req.method} ${req.originalUrl}`, 404));
